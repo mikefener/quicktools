57c117b53ffcea7b72fdae71c642b3d5d554ba77
Z3RO <your-mikefener@gmail.com>
2026-09-04 08:31:43 +0300

    Add robots.txt configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,16 +1,11 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://quicktools.dev'
-
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/'],
-      },
-    ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://quicktoolsweb.com/sitemap.xml',
   }
 }
